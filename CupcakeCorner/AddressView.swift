92afc621c6b648db7cb876c734869993887bc169
Víctor Ávila <victoraavila2000@gmail.com>
2024-05-19 19:09:02 -0300

    Checking for a valid address

--- CupcakeCorner/AddressView.swift
+++ CupcakeCorner/AddressView.swift
@@ -7,11 +7,37 @@
 
 import SwiftUI
 
+// The next step will be to let the user enter the address data into a form.
+// We'll add some validation: the user will be able to proceed to the next step only if their address looks good.
+// It will have name, street address, city and zip/postal code. Also, it will have a NavigationLink to the next screen.
+
 struct AddressView: View {
-    var order: Order
+    // All three Views point to the same object of data
+    // The @State automatically creates 2-way bindings for us (exposed with $). We don't use @State here because we aren't making the order instance here.
+    // Internally, we know that Order uses the @Observable macro (so SwiftUI in theory watches it for changes)
+    // @Bindable is necessary in order to create the 2-way bindings to its mutable properties that couldn't be created because we couldn't use @State here. These bindings are able to work with @Observable objects.
+    @Bindable var order: Order
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        Form {
+            Section {
+                // This data will persist even if we leave this View and come back, because we are passing around the same class instance (if we had used structs or local data with @State we would lost it if we did that)
+                TextField("Name", text: $order.name)
+                TextField("Street Address", text: $order.streetAddress)
+                TextField("City", text: $order.city)
+                TextField("Zip", text: $order.zip)
+            }
+            
+            Section {
+                NavigationLink("Check out") {
+                    CheckoutView(order: order)
+                }
+            }
+            // Stop user from progressing to Check Out if all fields aren't filled.
+            .disabled(order.hasValidAddress == false)
+        }
+        .navigationTitle("Delivery details")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
--- CupcakeCorner/Order.swift
+++ CupcakeCorner/Order.swift
@@ -27,4 +27,20 @@ class Order {
     }
     var extraFrosting = false
     var addSprinkles = false
+    
+    // Fields that will be filled in by AddressView
+    var name = ""
+    var streetAddress = ""
+    var city = ""
+    var zip = ""
+    
+    // How to stop the user if they only filled in 3 out of the 4 fields?
+    // Please, don't use length checks for the name, for example. They exclude people.
+    // Let's just verify if name, street address, city and zip aren't empty. We'll combine this with SwiftUI .disabled() modifier to stop the user interaction if the condition is true.
+    var hasValidAddress: Bool {
+        if name.isEmpty || streetAddress.isEmpty || city.isEmpty || zip.isEmpty {
+            return false
+        }
+        return true
+    }
 }
